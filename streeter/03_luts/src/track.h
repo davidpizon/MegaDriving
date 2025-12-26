7ee3c29f49a306744c50ebd236cabae76b76dd1a
Greg Gallardo <greggallardo@gmail.com>
2025-12-25 23:32:19 -0600

    more pre-generated scroll vals

--- streeter/03_luts/src/track.h
+++ streeter/03_luts/src/track.h
@@ -6,9 +6,9 @@
 #define ROAD_START_LINE 144 
 #define ZMAP_LENGTH 80 
 
-#define POS_DATA_LEN 383 
+#define POS_DATA_LEN 767 
 const fastfix16 total_road_length = FASTFIX16( 19.190000 );
-const fastfix16 data_step_size = FASTFIX16( 0.050000 );
+const fastfix16 data_step_size = FASTFIX16( 0.025000 );
 
 extern s16 scroll_data[];
 extern u16 pos_to_scroll_data_offset[];
